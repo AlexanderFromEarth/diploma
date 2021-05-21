df3125e614fbe721973927a8aba8f89727998015
Alexander Andreeff <alexanderfromearth@gmail.com>
2021-05-22 00:51:13 +0500

    add model creation types

---
+++ src/core/constants/__init__.py
@@ -0,0 +1,2 @@
+from core.constants.command_types import Commands
+from core.constants.event_types import Events
---
+++ src/core/constants/event_types.py
@@ -0,0 +1,4 @@
+from enum import Enum, auto
+
+class Events(Enum):
+    MODEL_CREATED = auto()
